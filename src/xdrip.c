1aa0173fb1079dd1e78264d21f5d206f5b944a32
AdrianLxM <adrian.m@inbox.com>
2016-08-16 13:08:42 +0200

    move upper/lower face layer to fully fit the trend

--- src/xdrip.c
+++ src/xdrip.c
@@ -2167,9 +2167,9 @@ void window_load_cgm(Window *window_cgm)
 #ifdef DEBUG_LEVEL
 	APP_LOG(APP_LOG_LEVEL_INFO, "Creating Upper and Lower face panels");
 #endif
-	upper_face_layer = bitmap_layer_create(GRect(0,0,144,83));
+	upper_face_layer = bitmap_layer_create(GRect(0,0,144,88));
 	bitmap_layer_set_background_color(upper_face_layer, GColorWhite);
-	lower_face_layer = bitmap_layer_create(GRect(0,84,144,165));
+	lower_face_layer = bitmap_layer_create(GRect(0,89,144,165));
 #ifdef PBL_COLOR
 	bitmap_layer_set_background_color(lower_face_layer, GColorDukeBlue);
 #else
